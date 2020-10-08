72dfa60383b233327c8fe23c6a1d107d8c70ea45
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-07 20:27:41 -0400

    some syntatic mods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,15 +16,15 @@ class StudentsController < ApplicationController
 	end
 	
 	def create
-		@student = Student.new(student_params(:first_name, :last_name))
-		@student.save
-		redirect_to student_path(@student)
+		student = Student.new(student_params(:first_name, :last_name))
+		student.save
+		redirect_to student_path(student)
 	end
 
 	def update
-		@student = Student.find(params[:id])
-		@student.update(student_params(:first_name, :last_name))
-		redirect_to student_path(@student)
+		student = Student.find(params[:id])
+		student.update(student_params(:first_name, :last_name))
+		redirect_to student_path(student)
 	end
 
 	private
